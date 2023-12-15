2bcb3ce2427f8df0ace6c4ec6c2612ff7efd2051
Sachin-0099 <itssachin0099@gmail.com>
2023-12-15 23:35:42 +0530

    Initial commit

---
+++ sum1.java
@@ -0,0 +1,24 @@
+package Array;
+
+public class sum1 {
+    public static int sum(int arr[],int x){
+          
+    int count=0;
+    for(int i=0;i<arr.length;i++){
+        for(int j=i+1;j<arr.length;j++){
+            if(arr[i]+arr[j]==x){
+                count++;
+            }
+        }
+  
+ }
+  return count;
+    }
+    public static void main(String args []){
+
+   
+    int arr[]={ 1,3,6,2,5,4,3,2,4};
+      int x=12;
+System.out.println(sum(arr,x));
+    }
+}
